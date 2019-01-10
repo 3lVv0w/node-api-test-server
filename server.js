7cd0cad2fe0dcd348dea8aed3987d5a2a06f0c1f
3lVv0w <krseisenh@gmail.com>
2019-01-10 14:42:01 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.post('/test', (req, res) => {
+  console.log(req.body);
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
